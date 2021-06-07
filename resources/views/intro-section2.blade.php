0a97a087f1fda0824fd96ef04f3b29679c9eca9c
unknown <nick@shopifygenius.com>
2021-06-07 20:37:20 +0530

    first desiner changes

--- resources/views/intro-section2.blade.php
+++ resources/views/intro-section2.blade.php
@@ -22,34 +22,34 @@
  <div class="row"> <div class="col-sm-8">
  <div class="sec1-box-left">
  <div class="box-head">
- <div class="box-icon animate__animated animate__fadeInLeft"><img src="images/leaves.svg" /></div><h4 class="animate__animated animate__fadeIn">Climate change will demonstrably affect</h4>
+ <div class="box-icon animate__animated animate__fadeInLeft"><img src="images/leaves.svg" /></div><h4 class="animate__animated animate__fadeIn">Which straw do you think is the most sustainable?</h4>
  </div>
- <p class="animate__animated animate__fadeIn">Global warming is a serious problem all around the world, and rising temperatures<br>have shifted natural climatic patterns. Who do you think will feel these adverse<br> effects the most?</p>
+ <p class="animate__animated animate__fadeIn">The materials used to create these straws are all different from one another.<br>Choose the material you think is the most environment friendly.</p>
 
 
   <form><div class="checkbox-group">
     <div class="form-group">
       <input type="checkbox" id="html">
-      <label for="html">My generation</label>
+      <label for="html">Wooden</label>
     </div>
     <div class="form-group">
       <input type="checkbox" id="css">
-      <label for="css">My grandchildren</label>
+      <label for="css">Paper</label>
     </div>
     <div class="form-group">
       <input type="checkbox" id="javascript">
-      <label for="javascript">My children</label>
+      <label for="javascript">Plastic</label>
     </div>
 	 <div class="form-group">
       <input type="checkbox" id="java">
-      <label for="java">This not a serious threat</label>
+      <label for="java">Steel</label>
     </div></div>
   </form>
 
 </div>
  
  </div><div class="col-sm-4">
- <div class="sec1-box-right"><img src="images/burning-earth.svg"/></div>
+ <div class="sec1-box-right"><img src="images/straw.svg"/></div>
  </div></div>
 <div class="button-wrapper bottom-button">
 <button>Next</button>
