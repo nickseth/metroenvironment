c336e9490a5228f3d0cd20f9094a56a6cd944bad
unknown <nick@shopifygenius.com>
2021-06-09 19:15:09 +0530

    first designer changes

--- resources/views/personal-level-start.blade.php
+++ resources/views/personal-level-start.blade.php
@@ -17,4 +17,7 @@
      
     </div>
   </div>
+  <div class="button-wrapper bottom-button "><div class="container text-right">
+<a href="{{url('/intro-section3')}}" class="link-btn hvr-forward get-survey-details"><i class="fa fa-arrow-right fa-3x"></i></a>
+</div></div>
 </body>
