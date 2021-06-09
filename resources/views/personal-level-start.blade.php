14f9bdf97ec2e89eefbed3e0dea44441cc27ca58
unknown <nick@shopifygenius.com>
2021-06-09 18:51:07 +0530

    first designer changes

--- resources/views/personal-level-start.blade.php
+++ resources/views/personal-level-start.blade.php
@@ -1,4 +1,20 @@
 @include('header')
 <body class="personal-level-start">
+<nav class="navbar navbar-default">
+  <div class="container"><div class="d-flex align-center space-between">
+    <div class="navbar-header">
 
+      <a class="navbar-brand" href="#"><img src="images/metro-logo.svg" /></a>
+    </div>
+    <div class=" navbar-collapse right-header text-right" id="myNavbar">
+      <h1>#<span>M</span>vironment</h1>
+    </div></div>
+  </div>
+</nav>
+
+  <div class="parent-zoom" onclick="">
+    <div class="child-bg bg-one">
+     
+    </div>
+  </div>
 </body>
