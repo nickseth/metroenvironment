c1d8cf390a70dc8c56b2563160d78c260cfb4b0e
unknown <nick@shopifygenius.com>
2021-06-11 12:03:36 +0530

    step21 add url

---
+++ resources/views/personal-level-step21.blade.php
@@ -0,0 +1,22 @@
+@include('header')
+<body class="personal-level-step1 personal-level-step">
+<nav class="navbar navbar-default">
+  <div class="container"><div class="d-flex align-center space-between">
+    <div class="navbar-header">
+
+      <a class="navbar-brand" href="#"><img src="images/metro-logo.svg" /></a>
+    </div>
+    <div class=" navbar-collapse right-header text-right" id="myNavbar">
+      <h1>#<span>M</span>vironment</h1>
+    </div></div>
+  </div>
+</nav>
+
+<div class="main-content-wrapper">
+<div class="circular-range-slider"></div>
+  <div class="button-wrapper bottom-button "><div class="container text-right">
+  
+<a href="{{url('/intro-section3')}}" class="link-btn hvr-forward get-survey-details"><i class="fa fa-arrow-right fa-3x"></i></a>
+</div></div>
+</div>
+</body>
