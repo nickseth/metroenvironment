06df62827054891794db495c5277f2bf538ad044
unknown <nick@shopifygenius.com>
2021-06-11 13:04:13 +0530

    add css into custom.css

--- resources/views/personal-level-step21.blade.php
+++ resources/views/personal-level-step21.blade.php
@@ -2,44 +2,67 @@
 <body class="sec1-bg">
 <!-- Navbar -->
 <nav class="navbar navbar-default">
-  <div class="container"><div class="d-flex align-center space-between">
-    <div class="navbar-header">
+<div class="container"><div class="d-flex align-center space-between">
+  <div class="navbar-header">
 
-      <a class="navbar-brand" href="#"><img src="images/metro-logo.svg" /></a>
-    </div>
-    <div class=" navbar-collapse right-header text-right" id="myNavbar">
-      <h1>#<span>M</span>vironment</h1>
-    </div></div>
+    <a class="navbar-brand" href="#"><img src="images/metro-logo.svg" /></a>
   </div>
+  <div class=" navbar-collapse right-header text-right" id="myNavbar">
+    <h1>#<span>M</span>vironment</h1>
+  </div></div>
+</div>
 </nav>
 
 <div class="container "><div class="relative">
 
- 
- <div class="page-width mt-7">
- <div class="row">
 
-  <div class="col-sm-12">
+<div class="page-width mt-7">
+<div class="row">
 
- <div class="sec1-box-center text-center">
- <div class="box-top">
- <div class="box-icon animate__animated animate__fadeInLeft"><img src="images/leaves.svg" /></div>
- <div class="animate__animated animate__fadeIn green-text">Which of the following steps have you already adopted in your everyday life?</div>
- </div>
- <p class="animate__animated animate__fadeIn gray-text">Choose the boxes for the initiatives that you have embraced in your daily routine.</p>
+<div class="col-sm-12">
+
+<div class="sec1-box-center text-center">
+<div class="box-top">
+<div class="box-icon animate__animated animate__fadeInLeft"><img src="images/leaves.svg" /></div>
+<div class="animate__animated animate__fadeIn green-text">Which of the following steps have you already adopted in your everyday life?</div>
+</div>
+<p class="animate__animated animate__fadeIn gray-text">Choose the boxes for the initiatives that you have embraced in your daily routine.</p>
 
 </div>
- 
- </div>
- <div class="col-sm-12">
- <div class="sec1-box-right animate__animated animate__fadeInRight"><div class="bird-anim"><img src="images/bird-anim.gif" /></div>
- 
- </div>
- </div></div>
+
+</div>
+<div class="col-sm-12">
+<div class="sec1-box-right animate__animated animate__zoomIn">
+<div class="grid-container animate__zoomIn">
+<div class="grid-item"> 
+<div class="grid-icons"></div>
+<p class="light-text">Water conservation </p>
+</div>
+<div class="grid-item">Energy
+conservation</div>
+<div class="grid-item">Garbage
+saving</div>  
+<div class="grid-item">Vegetarian/vegan
+diet</div>
+<div class="grid-item">Conscious mobility
+(bicycle, public transport,
+e-mobility)</div>
+<div class="grid-item">Reduced
+consumption</div>  
+<div class="grid-item">Repair/Refurbishment</div>
+<div class="grid-item">Minimising
+food waste</div>
+<div class="grid-item">Open field for
+own addition
+</div>  
+</div>
+
+</div>
+</div></div>
 <div class="button-wrapper bottom-button">
 <a href="{{url('/intro-section2')}}" class="link-btn hvr-forward get-survey-details"><i class="fa fa-arrow-right fa-3x"></i></a>
 </div>
- </div>
- </div></div>
+</div>
+</div></div>
 </body>
 </html>
