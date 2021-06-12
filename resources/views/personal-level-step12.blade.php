5ae2a86d08d893c33a272b6349742269b64e50ad
unknown <nick@shopifygenius.com>
2021-06-12 16:48:58 +0530

    first designer code

---
+++ resources/views/personal-level-step12.blade.php
@@ -0,0 +1,63 @@
+@include('header')
+<body class="personal-level-step12 personal-level-step">
+<nav class="navbar navbar-default">
+  <div class="container"><div class="d-flex align-center space-between">
+    <div class="navbar-header">
+
+      <a class="navbar-brand" href="{{URL::to('/')}}"><img src="images/metro-logo.svg" /></a>
+    </div>
+    <div class=" navbar-collapse right-header text-right" id="myNavbar">
+      <h1>#<span>M</span>vironment</h1>
+    </div></div>
+  </div>
+</nav>
+
+
+
+<div class="container "><div class="relative">
+
+ 
+ <div class="page-width mt-7">
+ <div class="row">
+
+  <div class="col-sm-12">
+
+ <div class="sec1-box-center text-center">
+ <div class="box-top">
+ 
+ <div class="animate__animated animate__fadeIn green-text">Which of the following alternative protein sources do you use in your diet?</div>
+ </div>
+ 
+
+</div>
+ 
+ </div>
+ <div class="col-sm-12">
+ <div class="vegi-grup-wrap mt-7">
+<div class="vegi-group">
+ <div class="row"> <div class="col-sm-3"><p class="text-center"><strong>Tofu/soy products</strong>(soy cutlets, seitan, tempeh etc.)</p></div>
+<div class="col-sm-6">
+ 
+ <div class="vegi-group-imgs">
+ <div class="plate-item-1"><img src="images/plate-item-1.svg" /></div>
+ <div class="plate-item-2"><img src="images/plate-item-2.svg" /></div>
+ <div class="plate-item-3"><img src="images/plate-item-3.svg" /></div>
+ <div class="plate-item-4"><img src="images/plate-item-4.svg" /></div>
+ </div>
+ 
+ </div><div class="col-sm-3"><p class="text-center"><strong>Meat alternatives</strong>(Veggie sausage, mince, burger.)</p></div></div>
+ <div class="row"><div class="col-sm-3"><p class="text-center"><strong>Legumes</strong>(lentils, chickpeas, kidney beans, peas etc.)</p></div><div class="col-sm-6"><p class="text-center"><strong>Nuts/seeds</strong>(pumpkin- sunflower seeds, peanuts, cashew etc.)</p></div><div class="col-sm-3"><p class="text-center"><strong>Others</strong>(please mention)</p></div></div>
+ </div>
+</div>
+<div class="vegi-plate"></div>
+</div>
+ </div></div>
+<div class="button-wrapper bottom-button">
+<a href="{{url('/personal-level-step4')}}" class="link-btn hvr-forward get-survey-details"><i class="fa fa-arrow-right fa-3x"></i></a>
+</div>
+ </div>
+ </div></div>
+
+
+
+</body>
