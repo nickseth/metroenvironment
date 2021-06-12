3840b27abac7e40a64d111a244c63c4356486a76
unknown <nick@shopifygenius.com>
2021-06-12 13:41:14 +0530

    first designer code

---
+++ resources/views/personal-level-step20.blade.php
@@ -0,0 +1,49 @@
+@include('header')
+<body class="personal-level-step20 personal-level-step">
+<nav class="navbar navbar-default">
+  <div class="container"><div class="d-flex align-center space-between">
+    <div class="navbar-header">
+
+      <a class="navbar-brand" href="{{URL::to('/')}}"><img src="images/metro-logo.svg" /></a>
+    </div>
+    <div class=" navbar-collapse right-header text-right" id="myNavbar">
+      <h1>#<span>M</span>vironment</h1>
+    </div></div>
+  </div>
+</nav>
+
+
+
+<div class="container "><div class="relative">
+
+ 
+ <div class="page-width mt-7">
+ <div class="row">
+
+  <div class="col-sm-12">
+
+ <div class="sec1-box-center text-center">
+ <div class="box-top">
+ <div class="box-icon animate__animated animate__fadeInLeft"><img src="images/leaves.svg" /></div>
+ <div class="animate__animated animate__fadeIn green-text">I am willing to pay a higher price for products that cause less environmental pollution, and are fairly traded</div>
+ </div>
+ <p class="animate__animated animate__fadeIn gray-text">Move the slider on the circle on a scale of 1 to 4 to submit your response, where 1 = least likely and 4 = most likely.</p>
+
+</div>
+ 
+ </div>
+ <div class="col-sm-12">
+<form class="survey-form hide">
+<input type="hidden" class="hidden-circular-slider-input" value="0" name="buy_locally_grown_fruit"/>
+</form>
+<div class="circular-range-slider"></div>
+ </div></div>
+<div class="button-wrapper bottom-button">
+<a href="{{url('/personal-level-step4')}}" class="link-btn hvr-forward get-survey-details"><i class="fa fa-arrow-right fa-3x"></i></a>
+</div>
+ </div>
+ </div></div>
+
+
+
+</body>
