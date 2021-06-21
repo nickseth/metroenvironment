b48140a63e540df6cf14820deba7173a6da67e97
Anand Kumar <nick@shopifygenius.com>
2021-06-21 14:46:24 +0530

    change

--- resources/views/personal-level-step21.blade.php
+++ resources/views/personal-level-step21.blade.php
@@ -11,7 +11,7 @@
 					</a>
 				</div>
 				<div class=" navbar-collapse right-header text-right" id="myNavbar">
-					<h1>#<span>M</span>vironment</h1><strong>Personal level</strong>
+					<h1>#<span>M</span>vironment</h1><strong>SUSTAINABILITY & YOU</strong>
 				</div>
 			</div>
 		</div>
