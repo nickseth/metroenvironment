200d00927c629fc112d8e14f47897dc824c9eb32
unknown <nick@shopifygenius.com>
2021-06-22 13:50:34 +0530

    first designer code

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}" data-base-path="{{URL::to('/')}}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MVIRONMENT</title>
+		      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link href="css/style.css?<?php echo rand(1,1000000);?>" rel="stylesheet">
+
+</head>
+
+<body>
+ <h1>This website is available in the following languages:</h1>
+</body>
+</html>
