d3a5c29f9ec28f78e391f35c8d8f1645a3d37ca4
unknown <nick@shopifygenius.com>
2021-06-22 14:11:25 +0530

    first designer code

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -11,7 +11,17 @@
 
 </head>
 
-<body>
- <h1>This website is available in the following languages:</h1>
+<body class="landing"><div class="landing-box">
+ <h2 class="text-center">This website is available in the following languages:</h2>
+ <div class="lang-btn text-center"><a href="https://bdevtestserver.com/metro/public/" class="btn-flip index-btn" data-back="English" data-front="English"></a>
+<a href="https://bdevtestserver.com/metro/german/public/" class="btn-flip index-btn" data-back="GERMAN" data-front="GERMAN"></a>
+</div>
+<p>
+Alternatively, if you want to view the website in another language, you can avail of the translation feature in Google Chrome. To know more, go to the following link:<br/><br/><a href="https://support.google.com/chrome/answer/173424?co=GENIE.Platform%3DDesktop&hl=en" >
+https://support.google.com/chrome/answer/173424?co=GENIE.Platform%3DDesktop&hl=en</a>
+<br/><br/>
+<em><strong>Note:</strong> Google Translate cannot translate all content, and may not provide an exact translation. Anyone relying on information obtained from using Google Translate does so at his or her own risk.<em>
+
+</p></div>
 </body>
 </html>
