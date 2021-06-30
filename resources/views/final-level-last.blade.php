26247ffe4aecafd6cd08ac8c64afa665751297d0
unknown <nick@shopifygenius.com>
2021-06-30 16:49:51 +0530

    first designer code

--- resources/views/final-level-last.blade.php
+++ resources/views/final-level-last.blade.php
@@ -22,7 +22,8 @@
 		<div class="relative">
 			<div class="page-width mt-1 page6">
 				<div class="row">
-					<div class="col-sm-12">
+					
+					<div class="col-sm-9">
 						<div class="sec1-box-center text-center mt-5">
 							<div class="box-top">
 
@@ -31,25 +32,42 @@
 							</div>
 							<p class="animate__animated animate__fadeIn  badge-owner">MVIRONMENT BADGE</p>
 						</div>
-					</div>
-					<div class="col-sm-12">
 						<div class="sec1-box-right animate__animated animate__fadeInUp ">
 							<div class="badges">
-								<div class="badge-1 badge-grid gold-badge hide">
+								<div class="badge-1 badge-grid gold-badge ">
 								<img src="images/gold_new.png" />
 								<div class="center-btn text-center"><a href="download/gold.zip" download class="btn-flip next-btn download-zip-btn" data-back="Download" data-front="Download"></a></div>
 								</div>
-								<div class="badge-2 badge-grid silver-badge hide">
+								<div class="badge-2 badge-grid silver-badge ">
 								<img src="images/silver_new.png" />
 								<div class="center-btn text-center"><a href="download/silver.zip" download class="btn-flip next-btn download-zip-btn" data-back="Download" data-front="Download"></a></div>
 								</div>
-								<div class="badge-3 badge-grid bronze-badge hide">
+								<div class="badge-3 badge-grid bronze-badge ">
 								<img src="images/bronze.png" />
 								<div class="center-btn text-center"><a href="download/bronze.zip" download class="btn-flip next-btn download-zip-btn" data-back="Download" data-front="Download"></a></div>
 								</div>
 							</div>
 							</div>
 						</div>
+						<div class="col-sm-3"><div class="mt-5 badge-info"><div class="badge-info-box">
+						<pre>Did you enjoy
+taking #MViroment?
+
+We’ll be delighted to hear
+your opinion about it!
+
+Please share your feedback
+with <a href = "mailto: CR@metro.de"><strong>CR@metro.de</strong></a>
+
+Additionally, you can also
+share your Sustainability
+stories, innovation and
+inspiring work with us.
+
+Follow us on
+<strong>UNITED</strong>
+(M-Corprate Responsibility)</pre><div class="icon-badges"><img src="images/sustainable-badges.png" /></div>
+						</div></div></div>
 					
 				</div>
 			</div>
