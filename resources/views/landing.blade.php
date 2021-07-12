8c00e6e09cb39b4592198e7bc1e0b849a09bc94b
Anand Kumar <nick@shopifygenius.com>
2021-07-12 15:51:23 +0530

    changes

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -16,7 +16,7 @@
 
 <body class="landing language-page"><div class="landing-box">
 <div class="loader-wrap"><div class="loader"><div class="loadersmall"></div></div></div>
- <h2 class="text-center" style="text-transform:uppercase">This website is available in the following languages:</h2>
+ <h2 class="text-center google-head" style="text-transform:uppercase">This website is available in the following languages:</h2>
  <div class="lang-btn text-center"><a href="{{URL::to('/')}}" class="btn-flip translate-btn index-btn" data-language="en" data-back="English" data-front="English"></a>
 <a href="{{URL::to('/de')}}" class="btn-flip index-btn translate-btn" data-back="GERMAN " data-language="de" data-front="GERMAN"></a>
 </div>
@@ -24,7 +24,7 @@
 To view the #MVIRONMENT website in another language, you can use the automated translate option provided by your browser. However, these are automated translations and may not provide an exact translation. Therefore, kindly use this option only if it is necessary.
 <br/><br/><span class="text-center" style="display:block;">Get help here:<a href="https://support.google.com/chrome/answer/173424?co=GENIE.Platform%3DDesktop&hl=en" target="_blank">Chrome</a> | <a href="https://support.microsoft.com/en-us/topic/use-microsoft-translator-in-microsoft-edge-browser-4ad1c6cb-01a4-4227-be9d-a81e127fcb0b" target="_blank">Edge</a> | <a href="https://support.apple.com/en-in/guide/safari/ibrw646b2ca2/mac" target="_blank">Safari</a>
 </span><br/>
-1. This survey is best viewed on your PC in 1920x1080 resolution.<br/>
+1. This website is best viewed on your PC in 1920x1080 resolution.<br/>
 2. This website is NOT optimized for mobile viewing.<br/>
 3. Recommended browsers Google Chrome, Microsoft Edge, Safari.<br/>
 <em>
